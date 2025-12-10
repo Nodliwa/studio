ba7500f04617c2ab1a9a397e56e8cc5cb560b60f
Lunga Nodliwa <elkay40@gmail.com>
2025-12-10 20:58:44 +0000

    Under public folder, I have created images folder, and the file name is

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -5,7 +5,7 @@ export default function PageHeader() {
     <header className="container mx-auto px-4 py-8 md:px-8">
       <div className="flex flex-col items-center text-center">
         <Image
-          src="/logo.svg"
+          src="/images/brand2.png"
           alt="SimpliPlan Logo"
           width={280}
           height={70}
