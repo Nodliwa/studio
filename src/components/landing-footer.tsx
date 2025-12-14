56155784f11ff34427d4f0355de08d996fd6c4f6
Lunga Nodliwa <elkay40@gmail.com>
2025-12-14 08:28:42 +0000

    that is not x (formally Twitter icon) and also not tiktok icon please fi

--- src/components/landing-footer.tsx
+++ src/components/landing-footer.tsx
@@ -9,15 +9,15 @@ import { Facebook, Instagram } from 'lucide-react';
 import Link from 'next/link';
 
 const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M18 6 6 18" />
-        <path d="m6 6 12 12" />
+    <svg viewBox="0 0 1200 1227" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.163 519.284ZM569.165 687.828L521.697 619.934L144.011 79.6902H306.615L596.021 389.542L643.489 457.436L1029.51 1142.1H866.905L569.165 687.828Z" />
     </svg>
 );
 
+
 const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M21 8.25c-1.42 0-2.73.55-3.75 1.55v7.65a3.5 3.5 0 0 1-3.5 3.5h-1a3.5 3.5 0 0 1-3.5-3.5V5.5A3.5 3.5 0 0 1 9.75 2a3.5 3.5 0 0 1 3.5 3.5v9.1c0 .28.22.5.5.5s.5-.22.5-.5V8.25C14.25 4.79 11.46 2 8.25 2S2.25 4.79 2.25 8.25s2.79 6.25 6 6.25v-2.5c-2.07 0-3.75-1.68-3.75-3.75S4.18 4.5 6.25 4.5s3.75 1.68 3.75 3.75v.5h2.5V8.25c0-1.42.55-2.73 1.55-3.75C15.27 3.55 16.58 3 18 3c1.66 0 3 1.34 3 3v2.25Z" />
+     <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" {...props}>
+        <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-1.06-.6-1.97-1.46-2.65-2.49-.86-1.28-1.3-2.78-1.3-4.29 0-1.51.52-3.02 1.47-4.25 1.11-1.44 2.65-2.39 4.3-2.73.05-1.07.01-2.14-.02-3.21-1.22-.26-2.39-.75-3.36-1.52-1.26-1.02-2.08-2.45-2.34-4.04h4.03c.14 1.09.64 2.14 1.45 2.94.05.06.1.12.16.18z" />
     </svg>
 );
 
