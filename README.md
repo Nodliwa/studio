9f731d2fbbccb532f326d1d0bf29de2ce3699698
Lunga Nodliwa <elkay40@gmail.com>
2025-12-23 17:55:17 +0000

    If your Project ID changed, then: ❌ Your dev app is now talking to a dif

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ This is a NextJS starter in Firebase Studio.
 
 To get started, take a look at src/app/page.tsx.
 
-<!-- Final restoration build trigger v10 -->
-    
+<!-- Final restoration build trigger v12. Correcting project config. -->
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-1406892914-3d877",
+  "projectId": "studio-1406892914-3d877-dev",
   "appId": "1:1065920530402:web:9473c73135d2430dfe1bbb",
   "apiKey": "AIzaSyCszuF0EWsn1BYHKAtiViikJsWqVjhV6Zw",
-  "authDomain": "studio-1406892914-3d877.firebaseapp.com",
-  "storageBucket": "studio-1406892914-3d877.appspot.com",
+  "authDomain": "studio-1406892914-3d877-dev.firebaseapp.com",
+  "storageBucket": "studio-1406892914-3d877-dev.appspot.com",
   "messagingSenderId": "1065920530402",
   "measurementId": "G-J5BMEG1H0F"
 };
-
-    
