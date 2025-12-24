1f611d71ab590e7290f12e39502d8cbfa9a98e3b
Lunga Nodliwa <elkay40@gmail.com>
2025-12-24 13:37:48 +0000

    My correct project is studio-1406892914-3d877

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is a NextJS starter in Firebase Studio.
 
 To get started, take a look at src/app/page.tsx.
 
-<!-- Final deployment attempt trigger v6. Aligning all configs to the correct project ID. -->
+<!-- Final deployment attempt trigger v7. Aligning all configs to the correct project ID. -->
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "simpliplan-1d51a",
+  "projectId": "studio-1406892914-3d877",
   "appId": "1:1065920530402:web:9473c73135d2430dfe1bbb",
   "apiKey": "AIzaSyCszuF0EWsn1BYHKAtiViikJsWqVjhV6Zw",
-  "authDomain": "simpliplan-1d51a.firebaseapp.com",
-  "storageBucket": "simpliplan-1d51a.appspot.com",
+  "authDomain": "studio-1406892914-3d877.firebaseapp.com",
+  "storageBucket": "studio-1406892914-3d877.appspot.com",
   "messagingSenderId": "1065920530402",
   "measurementId": "G-J5BMEG1H0F"
 };
