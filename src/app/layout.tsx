987c666ceadb2ad633a61a467497cc71c29d2923
Lunga Nodliwa <elkay40@gmail.com>
2026-03-24 03:53:08 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { PT_Sans } from "next/font/google";
 import "./globals.css";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 const ptSans = PT_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -19,6 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={ptSans.className}>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17873532827"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17873532827');
+          `}
+        </Script>
+        
         <FirebaseClientProvider>
           {children}
           <Toaster />
