fdc3bfb109fdec013fd9b2d490a274b54e53fe6e
Lunga Nodliwa <elkay40@gmail.com>
2026-03-24 03:57:20 +0000

    add google measurement id G-5957KHVRVQ on every page of the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,13 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=AW-17873532827"
           strategy="afterInteractive"
         />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+        <Script id="google-tags-config" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-17873532827');
+            gtag('config', 'G-5957KHVRVQ');
           `}
         </Script>
         
